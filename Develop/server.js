20bad43eaf346f2dcbd3386a9a22c97e85acce43
Stephen Carlin <scarlinj@gmail.com>
2022-06-05 23:11:15 -0700

    site works, but does not add expenses. Fix this.

--- Develop/server.js
+++ Develop/server.js
@@ -18,11 +18,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, || 'mongodb://localhost:27017/budget_tracker', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+// mongoose.connect(MONGODB_URI, || 'mongodb://localhost:27017/budget_tracker', {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+// });
 
 // routes
 app.use(require("./routes/api.js"));
