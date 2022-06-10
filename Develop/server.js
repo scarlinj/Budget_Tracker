7a35765827db375a83e10ed03a2d784554343f3b
Stephen Carlin <scarlinj@gmail.com>
2022-06-10 00:13:23 -0700

    Added Mongoose connections

--- Develop/server.js
+++ Develop/server.js
@@ -18,11 +18,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, || 'mongodb://localhost:27017/budget_tracker', {
-//   useNewUrlParser: true,
-//   useFindAndModify: false,
-//   useUnifiedTopology: true
-// });
+// is 27017 for Mongoose?
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget_tracker', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
