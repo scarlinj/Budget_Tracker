438746c96e415a685f650ab06d0f85770ccb3606
Stephen Carlin <scarlinj@gmail.com>
2022-06-11 12:38:03 -0700

    removed other instances of 'morgan'

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget_track
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({
