d814ccbf662cb2208fcc70d5aefc781cd96133c9
Stephen Carlin <scarlinj@gmail.com>
2022-06-11 13:31:20 -0700

    deleted node_modules and package-lock.hson to troubleshoot

---
+++ Develop/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget_tracker";
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// is 27017 for Mongoose?
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget_tracker', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+app.use("./Develop/routes/api");
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
