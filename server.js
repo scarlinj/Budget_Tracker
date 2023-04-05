f9462dc5f2b6ab898a3585b944da0d644b752b80
Stephen Carlin <scarlinj@gmail.com>
2023-04-04 20:52:40 -0700

    updated localhost to 0.0.0.0 to fix ECONNREFUSED error.  This is due to Node.js update version 17.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget_tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0/budget_tracker";
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 
 // is 27017 for Mongoose?
   // removed 27017 from localhost below
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/budget_tracker', {
+mongoose.connect(MONGODB_URI || 'mongodb://0.0.0.0/budget_tracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
